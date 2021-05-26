4f21032de2a47447ed16382bcce5d23ae491a38a
Derek Honeycutt <derekshoneycutt@gmail.com>
2021-05-25 17:07:31 -0700

    Build updates

--- front_src/BuildSecuredFiles.ps1
+++ front_src/BuildSecuredFiles.ps1
@@ -3,7 +3,7 @@ Param(
 	[String] $AppInsightsKey
 )
 
-$useKey = '$AppInsightsKey'
+$useKey = "$AppInsightsKey"
 echo $useKey
 printf "`r`nimport `{ Imogene as `$ `} from '../Imogene/Imogene'; import { ApplicationInsights } from '@microsoft/applicationinsights-web'; `$`(`(`) => `{ const i = new ApplicationInsights`(`{ config: `{ instrumentationKey: '$useKey' `} `}); i.loadAppInsights(), i.trackPageView() `});`r`n" | Out-File "main/appinsights.js"
 cat "main\\appinsights.js"
