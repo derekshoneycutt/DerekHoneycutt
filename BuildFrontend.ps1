6c54949d9b97cd2b1ba0aa17908a0c8986f057fa
Derek Honeycutt <derekshoneycutt@gmail.com>
2021-05-26 17:27:57 -0700

    Some updates for building

--- BuildFrontend.ps1
+++ BuildFrontend.ps1
@@ -1,20 +1,33 @@
 
+Param(
+	[String] $AppInsightsKey
+)
+
+$useDir = './DerekHoneycutt.Frontend'
+
 echo "Cleaning Frontend Build"
-if (Test-Path -Path "./front_src/wwwroot/") {
-	Remove-Item -Path "./front_src/wwwroot/" -Recurse
+if (Test-Path -Path "$useDir/wwwroot/") {
+	Remove-Item -Path "$useDir/wwwroot/" -Recurse
 }
 echo "Prior Builds cleaned"
 
 
 echo "Preparing Build Directories"
-New-Item -Path "./front_src/wwwroot" -ItemType Directory
-New-Item -Path "./front_src/wwwroot/cmpcss" -ItemType Directory
-New-Item -Path "./front_src/wwwroot/cmpcss/sawtooth" -ItemType Directory
-Copy-Item -Path "./front_src/statics/*" -Destination "./front_src/wwwroot/" -Recurse
+New-Item -Path "$useDir/wwwroot" -ItemType Directory
+New-Item -Path "$useDir/wwwroot/cmpcss" -ItemType Directory
+New-Item -Path "$useDir/wwwroot/cmpcss/sawtooth" -ItemType Directory
+Copy-Item -Path "$useDir/statics/*" -Destination "$useDir/wwwroot/" -Recurse
 echo "Build Directories Prepared"
 
+if ($AppInsightsKey) {
+	echo "Creating Appinsights JS"
+	$setFile = "import { Imogene as x } from '../Imogene/Imogene';import { ApplicationInsights } from '@microsoft/applicationinsights-web'x(() => { const a = new ApplicationInsights({ config: { instrumentationKey: '$AppInsightsKey' } }); a.loadAppInsights(); a.trackPageView(); });"
+	echo "$setFile" > "$useDir/main/appinsights.js"
+	echo "Appinsights js created"
+}
+
 echo "Building Frontend"
-cd "./front_src/"
+cd "$useDir/"
 echo "Building frontend formally..."
 npm run wbp
 if ($LastExitCode -ne 0) {
