0b0e26eebc47972f2ff7fc2922c8569ebac97556
Derek Honeycutt <derekshoneycutt@gmail.com>
2021-05-26 19:42:36 -0700

    Stupid error

--- decrypt_appinsights.sh
+++ decrypt_appinsights.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$DEREKHONEYCUTT_APPINSIGHTS_PASSCODE" --output ./DerekHoneycutt.Frontend/main/appinsights.json ./DerekHoneycutt.Frontend/main/appinsights.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$DEREKHONEYCUTT_APPINSIGHTS_PASSCODE" --output ./DerekHoneycutt.Frontend/main/appinsights.js ./DerekHoneycutt.Frontend/main/appinsights.js.gpg
